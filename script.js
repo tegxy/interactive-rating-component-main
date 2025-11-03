4615130cd395819d93e739f9fb914b5c5065e962
Tega Lucky-Fiakpa <oghenetegalucky@gmail.com>
2025-11-03 18:24:11 +0000

    js updates

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 const rates = document.querySelectorAll(".button");
+const submit = document.querySelector(".submitButton");
+const main1 = document.querySelector(".main1");
+const main2 = document.querySelector(".main2");
+let updateText = document.querySelector(".text");
+let selectedText = null;
 
 rates.forEach((rate) => {
   rate.onclick = function () {
@@ -9,12 +14,16 @@ rates.forEach((rate) => {
     });
     rate.classList.toggle("clicked");
 
-    const selectedText = rate.textContent;
-
-    const submit = document.querySelector(".submitButton");
-
-    const updateText = document.querySelector(".text");
-
-    console.log(updateText);
+    selectedText = rate.textContent;
   };
 });
+
+submit.onclick = function () {
+  if (!selectedText) {
+    alert("Please select a rating!");
+    return;
+  }
+  main1.style.display = "none";
+  main2.style.display = "flex";
+  updateText.textContent = "You selected " + selectedText + " out of 5";
+};
